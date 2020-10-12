d43d67a1c3246e77d3611a3ead4e00cf705eb32e
Baris Tikir <brstkr@icloud.com>
2020-10-12 23:54:09 +0200

    Created Instruction Section Components :dizzy:

---
+++ src/components/content/Info-Instructions.vue
@@ -0,0 +1,20 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'info-instruction',
+    components: {
+
+    }
+}
+</script>
+
+<style lang="scss">
+@import "../../scss/variables";
+@import "../../scss/mixins";
+@import "../../scss/globals";
+
+
+</style>
---
+++ src/components/layouts/Instructions.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+
+<script>
+export default {
+    name:'instructions'
+}
+</script>
+
+<style lang="scss">
+@import "../../scss/variables";
+@import "../../scss/mixins";
+@import "../../scss/globals";
+
+</style>
